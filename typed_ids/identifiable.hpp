bf0d76f05e173f96641f451178396c978508c3cc
kevin <kevinchannon@gmail.com>
2022-04-26 19:47:55 +0100

    Add identifiable_mixin::set_id

--- typed_ids/identifiable.hpp
+++ typed_ids/identifiable.hpp
@@ -11,17 +11,19 @@ namespace typed {
 template < typename Base_T, typename Id_T >
 class identifiable_mixin {
   using _base_type = Base_T;
+
  public:
-  using id_type = id<_base_type, Id_T>;
+  using id_type = id< _base_type, Id_T >;
 
   constexpr explicit identifiable_mixin(id_type id) : _id{std::move(id)} {}
 
   [[nodiscard]] constexpr const id_type& id() const noexcept { return _id; }
+  void set_id(id_type id) noexcept { _id = std::move(id); }
 
  protected:
   ~identifiable_mixin() = default;
 
-   id_type _id;
+  id_type _id;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
--- typed_ids/identifiable_test.cpp
+++ typed_ids/identifiable_test.cpp
@@ -10,8 +10,7 @@ namespace {
 /// <summary>
 /// A sheep is identifiable and we use the identifiable_mixin to provide our id-related data and methods.
 /// </summary>
-struct Sheep : public typed::identifiable_mixin<Sheep, size_t>
-{
+struct Sheep : public typed::identifiable_mixin< Sheep, size_t > {
   constexpr Sheep(id_type id) : typed::identifiable_mixin< Sheep, size_t >{id} {}
 };
 
@@ -20,6 +19,12 @@ struct Sheep : public typed::identifiable_mixin<Sheep, size_t>
 TEST(IdentifiableTests, ConstructWithIdHasTheRightId) {
   constexpr auto x = Sheep{Sheep::id_type{1234}};
   ASSERT_EQ(Sheep::id_type{1234}, x.id());
+}
+
+TEST(IdentifiableTests, SettingId) {
+  auto x = Sheep{Sheep::id_type{4321}};
+  x.set_id(Sheep::id_type{5678});
+  ASSERT_EQ(Sheep::id_type{5678}, x.id());
 }
 
 ///////////////////////////////////////////////////////////////////////////////
