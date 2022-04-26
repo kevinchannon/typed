579be6d925afab9bcff1f9a9e4f58d266d04799e
kevin <kevinchannon@gmail.com>
2022-04-26 19:42:25 +0100

    Add identifiabe mixin

---
+++ typed_ids/identifiable.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "id.hpp"
+
+///////////////////////////////////////////////////////////////////////////////
+
+namespace typed {
+
+///////////////////////////////////////////////////////////////////////////////
+
+template < typename Base_T, typename Id_T >
+class identifiable_mixin {
+  using _base_type = Base_T;
+ public:
+  using id_type = id<_base_type, Id_T>;
+
+  constexpr explicit identifiable_mixin(id_type id) : _id{std::move(id)} {}
+
+  [[nodiscard]] constexpr const id_type& id() const noexcept { return _id; }
+
+ protected:
+  ~identifiable_mixin() = default;
+
+   id_type _id;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+}  // namespace typed
+
+///////////////////////////////////////////////////////////////////////////////
