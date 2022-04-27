a079e8f53d93249a41465b0a8fa8ca271266d2a2
kevin <kevinchannon@gmail.com>
2022-04-27 07:51:08 +0100

    size and count return something that is directly comparable with the index_type

--- typed_ids/identifiable_item_collection.hpp
+++ typed_ids/identifiable_item_collection.hpp
@@ -15,16 +15,19 @@ namespace typed {
 
 template < typename Item_T, typename Index_T = size_t >
 class identifiable_item_collection {
-  using _this_type = identifiable_item_collection< Item_T, Index_T >;
+  using _this_type      = identifiable_item_collection< Item_T, Index_T >;
   using _container_type = std::vector< std::unique_ptr< Item_T > >;
 
  public:
   using value_type = Item_T;
   using index_type = index< _this_type, Index_T >;
-  using size_type  = typename index_type::value_type;
+  using size_type  = index_type;
   using id_type    = typename value_type::id_type;
 
-  [[nodiscard]] constexpr size_type size() const noexcept { return static_cast< size_type >(_values.size()); }
+  [[nodiscard]] constexpr size_type size() const noexcept {
+    return size_type{static_cast< typename size_type::value_type >(_values.size())};
+  }
+
   [[nodiscard]] constexpr size_type count() const noexcept { return size(); }
 
   value_type* const add(std::unique_ptr< value_type >&& val) {
@@ -47,16 +50,16 @@ class identifiable_item_collection {
       return nullptr;
     }
 
-    auto out  = std::move(_values[idx]);
+    auto out = std::move(_values[idx]);
     _values.erase(std::next(_values.begin(), idx));
 
     return out;
   }
 
  private:
-
   typename _container_type::size_type _find_index(const id_type& id) const {
-    return std::distance(_values.begin(), std::find_if(_values.begin(), _values.end(), [&id](auto&& val) { return val->id() == id; }));
+    return std::distance(_values.begin(),
+                         std::find_if(_values.begin(), _values.end(), [&id](auto&& val) { return val->id() == id; }));
   }
 
   _container_type _values;
